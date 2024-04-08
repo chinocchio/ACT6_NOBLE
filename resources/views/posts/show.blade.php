a7263b2d00ec9f10cc554b40a1227ba27d5008d3
chino <chinonoble07@gmail.com>
2024-04-08 22:52:17 +0800

    Add posts

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Posts</title>
+</head>
+<body>
+    <h1> This is my posts {{$post}}</h1>
+</body>
+</html>
